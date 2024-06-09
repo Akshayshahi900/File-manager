c9a1898fbf4a9e26f37a7980b296d6816c17bf3c
Akshay shahi <157788815+Akshayshahi900@users.noreply.github.com>
2024-06-09 12:00:08 +0530

    Uploaded the project

---
+++ index.js
@@ -0,0 +1,45 @@
+// You have to write a Node.js program to clear clutter inside of a directory and organize the contents of that directory into different folders
+
+// for example, these files become:
+
+// 1. name.jpg
+// 2. name.png
+// 3. this.pdf 
+// 4. harry.zip
+// 5. Rohan.zip
+// 6. cat.jpg 
+// 7. harry.pdf
+
+// this: 
+// jpg/name.jpg, jpg/cat.jpg 
+// png/name.png 
+// pdf/this.pdf pdf/harry.pdf
+// zip/harry.zip zip/Rohan.zip
+
+import fs from "fs/promises";
+import fsn from "fs"
+import path from "path"
+
+const basepath = "C:\\Sigma Webdevelopment\\vid93"
+let files = await fs.readdir(basepath)
+
+console.log(files)
+
+for (const item of files) {
+    let ext =item.split(".")[item.split(".").length -1]
+    console.log(`Final item name : ${ext[0]}`)
+    if (ext != "js" && ext != "json" && item.split(".").length > 1) {
+
+
+        if (fsn.existsSync(path.join(basepath, ext))) {
+            // Move the file to this directory if its not a js or json file
+            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+        }
+        else {
+            fs.mkdir(ext)
+            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+        }
+    }
+
+    
+}
